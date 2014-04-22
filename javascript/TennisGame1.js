80b4fda0425f7f61b093eff675f211aab1209c06
rakeshkumar007 <rakesh.kummar@gmail.com>
2014-04-21 22:30:28 -0700

    added js file

--- javascript/TennisGame1.js
+++ javascript/TennisGame1.js
@@ -1,67 +1,81 @@
-var TennisGame1 = function(player1Name, player2Name) {
-    this.m_score1 = 0;
-    this.m_score2 = 0;
-    this.player1Name = player1Name;
-    this.player2Name = player2Name;
+var TennisGame1 = function() {
+	this.firstPlayerScore = 0;
+	this.secondPlayerScore = 0;
 };
 
 TennisGame1.prototype.wonPoint = function(playerName) {
-    if (playerName === "player1")
-        this.m_score1 += 1;
-    else
-        this.m_score2 += 1;
+	if (playerName === "player1")
+		this.firstPlayerScore += 1;
+	else
+		this.secondPlayerScore += 1;
 };
 
+function tieScore() {
+	switch (this.firstPlayerScore) {
+	case 0:
+		return "Love-All";
+	case 1:
+		return "Fifteen-All";
+	case 2:
+		return "Thirty-All";
+	}
+
+	return "Deuce";
+};
+
+function scoreFourOnwards() {
+
+	var scoreDiff = this.firstPlayerScore - this.secondPlayerScore;
+	if (scoreDiff === 1)
+		return "Advantage player1";
+	else if (scoreDiff === -1)
+		return "Advantage player2";
+	else if (scoreDiff >= 2)
+		return "Win for player1";
+
+	return "Win for player2";
+
+};
+
+function tempMapping(tempScore) {
+	switch (tempScore) {
+	case 0:
+		return "Love";
+	case 1:
+		return "Fifteen";
+	case 2:
+		return "Thirty";
+	case 3:
+		return "Forty";
+	}
+
+}
+
 TennisGame1.prototype.getScore = function() {
-    var score = "";
-    var tempScore = 0;
-    if (this.m_score1 === this.m_score2) {
-        switch (this.m_score1) {
-            case 0:
-                score = "Love-All";
-                break;
-            case 1:
-                score = "Fifteen-All";
-                break;
-            case 2:
-                score = "Thirty-All";
-                break;
-            default:
-                score = "Deuce";
-                break;
-        }
-    } else if (this.m_score1 >= 4 || this.m_score2 >= 4) {
-        var minusResult = this.m_score1 - this.m_score2;
-        if (minusResult === 1) score = "Advantage player1";
-        else if (minusResult === -1) score = "Advantage player2";
-        else if (minusResult >= 2) score = "Win for player1";
-        else score = "Win for player2";
-    } else {
-        for (var i = 1; i < 3; i++) {
-            if (i === 1) tempScore = this.m_score1;
-            else {
-                score += "-";
-                tempScore = this.m_score2;
-            }
-            switch (tempScore) {
-                case 0:
-                    score += "Love";
-                    break;
-                case 1:
-                    score += "Fifteen";
-                    break;
-                case 2:
-                    score += "Thirty";
-                    break;
-                case 3:
-                    score += "Forty";
-                    break;
-            }
-        }
-    }
-    return score;
+	var score = "";
+	var tempScore = 0;
+	if (this.firstPlayerScore === this.secondPlayerScore) {
+
+		score = tieScore.call(this);
+
+	} else if (this.firstPlayerScore >= 4 || this.secondPlayerScore >= 4) {
+
+		score = scoreFourOnwards.call(this);
+
+	} else {
+		for ( var i = 1; i < 3; i++) {
+			if (i === 1)
+				tempScore = this.firstPlayerScore;
+			else {
+				score += "-";
+				tempScore = this.secondPlayerScore;
+			}
+			score += tempMapping(tempScore);
+		}
+	}
+	return score;
 };
 
 if (typeof window === "undefined") {
-    module.exports = TennisGame1;
+	module.exports = TennisGame1;
 }
---
+++ javascript/Tennisgame.js
@@ -0,0 +1,81 @@
+var TennisGame1 = function() {
+	this.firstPlayerScore = 0;
+	this.secondPlayerScore = 0;
+};
+
+TennisGame1.prototype.wonPoint = function(playerName) {
+	if (playerName === "player1")
+		this.firstPlayerScore += 1;
+	else
+		this.secondPlayerScore += 1;
+};
+
+function tieScore() {
+	switch (this.firstPlayerScore) {
+	case 0:
+		return "Love-All";
+	case 1:
+		return "Fifteen-All";
+	case 2:
+		return "Thirty-All";
+	}
+
+	return "Deuce";
+};
+
+function scoreFourOnwards(score) {
+
+	var scoreDiff = this.firstPlayerScore - this.secondPlayerScore;
+	if (scoreDiff === 1)
+		return "Advantage player1";
+	else if (scoreDiff === -1)
+		return "Advantage player2";
+	else if (scoreDiff >= 2)
+		return "Win for player1";
+
+	return "Win for player2";
+
+};
+
+function tempMapping(tempScore) {
+	switch (tempScore) {
+	case 0:
+		return "Love";
+	case 1:
+		return "Fifteen";
+	case 2:
+		return "Thirty";
+	case 3:
+		return "Forty";
+	}
+
+}
+
+TennisGame1.prototype.getScore = function() {
+	var score = "";
+	var tempScore = 0;
+	if (this.firstPlayerScore === this.secondPlayerScore) {
+
+		score = tieScore.call(this, score);
+
+	} else if (this.firstPlayerScore >= 4 || this.secondPlayerScore >= 4) {
+
+		score = scoreFourOnwards.call(this, score);
+
+	} else {
+		for ( var i = 1; i < 3; i++) {
+			if (i === 1)
+				tempScore = this.firstPlayerScore;
+			else {
+				score += "-";
+				tempScore = this.secondPlayerScore;
+			}
+			score += tempMapping(tempScore);
+		}
+	}
+	return score;
+};
+
+if (typeof window === "undefined") {
+	module.exports = TennisGame1;
+}
